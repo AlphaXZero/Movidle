833c4c65f54971b35ddc8351758db8ba3e23babe
gg <georgevanderveen@yahoo.fr>
2026-03-20 22:26:02 +0100

    Edited db part 3

--- Program.cs
+++ Program.cs
@@ -31,5 +31,9 @@ app.UseAntiforgery();
 app.MapStaticAssets();
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 app.Run();
